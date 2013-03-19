19112ba9b008e99f9a4ea2423bf421c37586f6fc
Kausikram Krishnasayee <kausikram@gmail.com>
2013-03-19 17:12:14 +0530

    auth details for basic auth now got from config file. password needs to be salted and sha1 hashed and only then inserted on the config file

--- merlin/system/middlewares/basic_auth_middleware.php
+++ merlin/system/middlewares/basic_auth_middleware.php
@@ -1,24 +1,36 @@
 <?php
 namespace merlin\middlewares\basic_auth_middleware;
 
+function _force_authenticate(){
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'This Url Requires Authentication';
+    exit;
+}
+
 function process_request(&$req){
-    $username="admin";
-    $password="admin";
+    $pairs = \merlin\config\get_config_item("basic_auth_pairs");
     if($req->requires_authentication()){
         $auth_user = $req->server('PHP_AUTH_USER');
         if (!isset($auth_user)) {
-            header('WWW-Authenticate: Basic realm="My Realm"');
-            header('HTTP/1.0 401 Unauthorized');
-            echo 'This Url Requires Authentication';
-            exit;
+            #auth creds not sent
+            _force_authenticate();
         } else {
-            if(($auth_user==$username) && ($req->server('PHP_AUTH_PW')==$password)){
-                return;
+            if(!isset($pairs[$auth_user])){
+                #creds for user not present
+                _force_authenticate();
             } else {
-                header('WWW-Authenticate: Basic realm="My Realm"');
-                header('HTTP/1.0 401 Unauthorized');
-                echo 'This Url Requires Authentication';
-                exit;
+                #creds for user present
+                $salted_password_hash = $pairs[$auth_user];
+                $split_password = explode("-",$salted_password_hash);
+                $salt = $split_password[0];
+                $password_hash = $split_password[1];
+
+                $hash_of_sent_creds = sha1($salt.$req->server("PHP_AUTH_PW"));
+                if($password_hash!=$hash_of_sent_creds){
+                    #creds are wrong
+                    _force_authenticate();
+                }
             }
         }
     }
