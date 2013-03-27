38b0471324ec44e0cc67be41fbba71851dd449ca
Kausikram Krishnasayee <kausikram@gmail.com>
2013-03-27 08:44:01 +0530

    added a direct from file generic controller

--- merlin/system/generic_controllers.php
+++ merlin/system/generic_controllers.php
@@ -6,3 +6,10 @@ function controller_404($req) {
     \header("HTTP/1.1 404 Not Found");
     include(\merlin\config\get_config_item("404_page"));
 }
+
+function direct_file_response($req) {
+    foreach($req->urlconfig("additional_headers") as $header){
+        header($header);
+    }
+    echo \file_get_contents($req->urlconfig("filename"));
+}
