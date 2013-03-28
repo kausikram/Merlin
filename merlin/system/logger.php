cf24bc7a997eefffc350a5a29e6982d4cebf3a2e
Kausikram Krishnasayee <kausikram@gmail.com>
2013-03-28 20:34:10 +0530

    logging now turned on based on configs

--- merlin/system/logger.php
+++ merlin/system/logger.php
@@ -4,5 +4,7 @@ namespace merlin\logger;
 
 function log($log_string) {
     //echo $log_string . "<br />";
-    //file_put_contents(\merlin\config\get_config_item("log_file"), $log_string."\n", FILE_APPEND);
+    if(\merlin\config\get_config_item("enable_logging")){
+        file_put_contents(\merlin\config\get_config_item("log_file"), $log_string."\n", FILE_APPEND);
+    }
 }
