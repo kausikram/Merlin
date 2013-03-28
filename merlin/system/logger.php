5f6d18d8cf577559024876c24f472fad9ed1f7db
Kausikram Krishnasayee <kausikram@gmail.com>
2013-03-28 20:24:12 +0530

    support for file based logging

--- merlin/system/logger.php
+++ merlin/system/logger.php
@@ -4,4 +4,5 @@ namespace merlin\logger;
 
 function log($log_string) {
     //echo $log_string . "<br />";
+    //file_put_contents(\merlin\config\get_config_item("log_file"), $log_string."\n", FILE_APPEND);
 }
--- merlin/system/middlewares/basic_auth_middleware.php
+++ merlin/system/middlewares/basic_auth_middleware.php
@@ -12,15 +12,19 @@ function process_request(&$req){
     \merlin\logger\log(" in merlin\\middlewares\\basic_auth_middleware\\process_request");
     $pairs = \merlin\config\get_config_item("basic_auth_pairs");
     if($req->requires_authentication()){
+        \merlin\logger\log("BasicAuthMiddleware: url requires authentication");
         $auth_user = $req->server('PHP_AUTH_USER');
         if (!isset($auth_user)) {
             #auth creds not sent
             _force_authenticate();
         } else {
+            \merlin\logger\log("BasicAuthMiddleware: user was set to: ".$auth_user);
             if(!isset($pairs[$auth_user])){
                 #creds for user not present
+                \merlin\logger\log("BasicAuthMiddleware: user not found");
                 _force_authenticate();
             } else {
+                \merlin\logger\log("BasicAuthMiddleware: processing password");
                 #creds for user present
                 $salted_password_hash = $pairs[$auth_user];
                 $split_password = explode("-",$salted_password_hash);
@@ -29,9 +33,11 @@ function process_request(&$req){
 
                 $hash_of_sent_creds = sha1($salt.$req->server("PHP_AUTH_PW"));
                 if($password_hash!=$hash_of_sent_creds){
+                    \merlin\logger\log("BasicAuthMiddleware: wrong password");
                     #creds are wrong
                     _force_authenticate();
                 }
+                \merlin\logger\log("BasicAuthMiddleware: password was correct let them go");
             }
         }
     }
